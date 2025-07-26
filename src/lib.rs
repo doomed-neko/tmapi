e4752f2a9458d5ccff979d2b8364d4282576425c
Adenosine <79870712+doomed-neko@users.noreply.github.com>
2025-07-26 15:29:53 +0300

    add crate-level docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+//! A simple library to help with using [vwh]'s TempMail service on <https://barid.site>
+//!
+//! ```no_run
+//! use tmapi::Client;
+//! # async{
+//! let client = Client::new("y@iusearch.lol").unwrap();
+//! //                              limit, offset
+//! let emails = client.get_emails(  10  ,   0   ).await.unwrap();
+//! let first_email = emails.iter().next().unwrap();
+//! let id = &first_email.id;
+//! client.delete_inbox(id).await.unwrap();
+//! # };
+//! ```
+//! [vwh]: <https://vwh.sh>
 mod client;
 mod types;
 
